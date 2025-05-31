bc11b106aa9713a809c2affffbc7407f02712da5
Sofiia Novychenko <milakotiy@gmail.com>
2025-05-31 12:00:19 +0200

    change validateBody error

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -5,7 +5,7 @@ export const validateBody = (schema) => async (req, resp, next) => {
     await schema.validateAsync(req.body, { abortEarly: false });
     next();
   } catch (err) {
-    const error = createHttpError(400, 'Bad request', { errors: err.details });
-    next(error);
+    const errors = err.details.map((detail) => detail.message);
+    next(createHttpError.BadRequest(errors));
   }
 };
